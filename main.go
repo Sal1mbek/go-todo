0398df6a563b2f8d04a5627196199cea00d40ced
Sal1mbek <salimbekkairbekov@gmail.com>
2024-03-08 14:37:23 +0500

    initial commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"errors"
+	"github.com/Sal1mbek/go-todo/routes"
+	"github.com/ichtrojan/thoth"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	logger, _ := thoth.Init("log")
+
+	if err := godotenv.Load(); err != nil {
+		logger.Log(errors.New("no .env file found"))
+		log.Fatal("No .env file found")
+	}
+
+	port, exist := os.LookupEnv("PORT")
+
+	if !exist {
+		logger.Log(errors.New("PORT not set in .env"))
+		log.Fatal("PORT not set in .env")
+	}
+
+	log.Print("starting server on :" + port)
+	err := http.ListenAndServe(":"+port, routes.Init())
+
+	if err != nil {
+		logger.Log(err)
+		log.Fatal(err)
+	}
+}
